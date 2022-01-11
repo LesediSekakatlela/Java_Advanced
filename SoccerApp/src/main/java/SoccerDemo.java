b0c80ff20a3bf0cd21ae2df70bd75fe51442b58b
Lesedi <leseditumelo32@gmail.com>
2022-01-11 11:11:31 +0200

    SoccerDemoApp

---
+++ SoccerApp/src/main/java/SoccerDemo.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class SoccerDemo {
+    public static void main(String[] args) {
+        // pass the path to the file as a parameter
+        File file = new File("C:\\Users\\admin\\Desktop\\java\\Java_Advanced\\SoccerApp\\src\\main\\resources\\results.txt");
+        Scanner sc = null;
+        String match, team1, team2;
+        Integer iscore1, iscore2;
+    }
+}
--- SoccerApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.lesedi.soccerapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.lesedi to javafx.fxml;
-    exports com.lesedi;
-}
